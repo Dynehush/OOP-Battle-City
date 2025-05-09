e813bdfafbc5da74ad9faaac0b8468b40f3b9052
Dynehush <156074834+Dynehush@users.noreply.github.com>
2025-05-09 16:09:45 +0200

    Add files via upload

---
+++ Battle City Beta 1.4 (for lab1)/Battle City/Entity/Player.cs
@@ -0,0 +1,186 @@
+﻿using Battle_City;
+using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Numerics;
+using System.Reflection.Emit;
+
+namespace Battle_City
+{
+    class Player : BaseObject
+    {
+        private static int _x = 2;
+        private static int _y = 2;
+        internal static string PlayerFigure = "→";
+        public static readonly string PlayerBullet = "0";
+
+        internal static readonly Dictionary<string, string> _playerDirection = new Dictionary<string, string>
+        {
+            {"up", "↑" },
+            {"down", "↓" },
+            {"left", "←" },
+            {"right", "→" }
+        };
+        public List<Bullet> Bullets { get; } = new List<Bullet>();
+
+        public new int X
+        {
+            get => _x;
+            set
+            {
+                if (value >= 0 && value < Field.Width)
+                {
+                    _x = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Player cannot move outside the field");
+                }
+            }
+        }
+
+        public new int Y
+        {
+            get => _y;
+            set
+            {
+                if (value >= 0 && value < Field.Height)
+                {
+                    _y = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Player cannot move outside the field");
+                }
+            }
+        }
+
+        public Player(Field _field) : base(_x, _y, PlayerFigure)
+        {
+            while (!_field.IsWalkable(X, Y))
+            {
+                X++;
+                if (X >= Field.Width)
+                {
+                    X = 2;
+                    Y++;
+                }
+            }
+        }
+
+        public void DrawToBuffer(char[,] buffer)
+        {
+            int bufferX = X;
+            int bufferY = Y;
+            if (bufferX >= 0 && bufferX < buffer.GetLength(0) && bufferY >= 0 && bufferY < buffer.GetLength(1))
+            {
+                buffer[bufferX, bufferY] = PlayerFigure[0];
+                if (PlayerFigure.Length > 1 && bufferX < buffer.GetLength(0))
+                    buffer[bufferX, bufferY] = PlayerFigure[1];
+            }
+        }
+
+        public override void Display() { Console.Write(PlayerFigure); }
+
+        public bool IsShot = false;
+        public static void PlayerActions(Player _player, Field _field, Reward _reward, Enemy _enemy)
+        {
+            var key = Console.ReadKey(true).Key;
+            bool isSpacebarPressed = false;
+            switch (key)
+            {
+                case ConsoleKey.W: _player.Move(0, -1, _field); PlayerFigure = _playerDirection["up"]; break;
+                case ConsoleKey.S: _player.Move(0, 1, _field); PlayerFigure = _playerDirection["down"]; break;
+                case ConsoleKey.A: _player.Move(-1, 0, _field); PlayerFigure = _playerDirection["left"]; break;
+                case ConsoleKey.D: _player.Move(1, 0, _field); PlayerFigure = _playerDirection["right"]; break;
+
+                case ConsoleKey.UpArrow: _player.Move(0, -1, _field); PlayerFigure = _playerDirection["up"]; break;
+                case ConsoleKey.DownArrow: _player.Move(0, 1, _field); PlayerFigure = _playerDirection["down"]; break;
+                case ConsoleKey.LeftArrow: _player.Move(-1, 0, _field); PlayerFigure = _playerDirection["left"]; break;
+                case ConsoleKey.RightArrow: _player.Move(1, 0, _field); PlayerFigure = _playerDirection["right"]; break;
+
+                case ConsoleKey.Spacebar: 
+                    if (!_player.IsShot)
+                        _player.Shoot(_field, _reward, _enemy, _player);
+                        _player.IsShot = true;
+                        isSpacebarPressed = true;
+                    break;
+
+                case ConsoleKey.F10: Game.GameState = Game.State.Save; break;
+
+                case ConsoleKey.Escape: Game.GameState = Game.State.Exit; break;
+            }
+            if (!isSpacebarPressed)
+            {
+                _player.IsShot = false; 
+            }
+        }
+
+        
+
+        public void Move(int dx, int dy, Field _field)
+        {
+            int newX = X + dx;
+            int newY = Y + dy;
+
+            if (newX >= Field.Width || newY >= Field.Height || newX < 0 || newY < 0)
+            {
+                Console.WriteLine("Player cannot move outside the field.");
+                return;
+            }
+
+            if (_field.IsWalkable(newX, newY))
+            {
+                X = newX;
+                Y = newY;
+            }
+        }
+
+        public void Shoot(Field _field, Reward _reward, Enemy _enemy, Player _player)
+        {
+            if (_player.X < _enemy.X && _player.Y < _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, 1, 1, PlayerBullet));
+            }
+            else if (_player.X > _enemy.X && _player.Y < _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, -1, 1, PlayerBullet));
+            }
+            else if (_player.X < _enemy.X && _player.Y > _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, 1, -1, PlayerBullet));
+            }
+            else if (_player.X > _enemy.X && _player.Y > _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, -1, -1, PlayerBullet));
+            }
+            else if (_player.X < _enemy.X && _player.Y == _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, 1, 0, PlayerBullet));
+            }
+            else if (_player.X > _enemy.X && _player.Y == _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, -1, 0, PlayerBullet));
+            }
+            else if (_player.X == _enemy.X && _player.Y > _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, 0, -1, PlayerBullet));
+            }
+            else if (_player.X == _enemy.X && _player.Y < _enemy.Y)
+            {
+                Bullets.Add(new Bullet(X, Y, 0, 1, PlayerBullet));
+            }
+        }
+
+
+        public static void CheckCollision(Player _player, Enemy _enemy)
+        {
+            if (_player.X == _enemy.X && _player.Y == _enemy.Y)
+            {
+                Game.GameState = Game.State.Pause;
+                Menu.ShowGameOver();
+            }
+        }
+    }
+}
---
+++ Battle City Beta 1.4 (for lab1)/Battle City/Game.cs
@@ -0,0 +1,400 @@
+﻿using Battle_City;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json;
+
+abstract class Game
+{
+    public static readonly Random Random = new Random();
+
+    public static int Level = 1;
+    private static int _score = 0;
+
+    public static readonly string heartSymbol = "❤️";
+    public static int Lives = 5;
+
+    private static Field _field = new Field(Level);
+    private static Enemy _enemy = new Enemy(_field);
+    private static Player _player = new Player(_field);
+    private static Reward _reward = new Reward(5);
+
+    private static Thread? _enemyMoveThread;
+    private static bool _enemyMoveActive = true;
+
+    private static Thread? _bulletThread;
+    private static bool _bulletActive = true;
+
+    private static Thread? _renderThread;
+    private static bool _renderActive = true;
+
+    private static readonly object consoleLock = new object();
+
+    private static char[,] renderBuffer = new char[Field.Width, Field.Height];
+
+    public enum State
+    {
+        Pause,
+        Run,
+        Win,
+        Defeat,
+        Save,
+        Exit,
+        Load
+    }
+
+    public static State GameState;
+
+    public static void ExecuteBasics()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.CursorVisible = false;
+        Menu.ShowMenu();
+
+        _reward.SetRewards(_score, _field, _player);
+        GameState = State.Run;
+
+        StartEnemyMovement();
+        StartBulletMovement();
+        StartRendering();
+
+        RunGame();
+    }
+
+    private static void RenderAll()
+    {
+        lock (consoleLock)
+        {
+            for (int y = 0; y < Field.Height; y++)
+                for (int x = 0; x < Field.Width; x++)
+                    renderBuffer[x, y] = ' ';
+
+            _field.DrawToBuffer(renderBuffer);
+            _player.DrawToBuffer(renderBuffer);
+            foreach (var bullet in _player.Bullets)
+                bullet.DrawToBuffer(renderBuffer);
+            foreach (var bullet in _enemy.Bullets)
+                bullet.DrawToBuffer(renderBuffer);
+            _enemy.DrawToBuffer(renderBuffer);
+
+            Console.SetCursorPosition(0, 0);
+            StringBuilder sb = new StringBuilder();
+            for (int y = 0; y < Field.Height; y++)
+            {
+                for (int x = 0; x < Field.Width; x++)
+                {
+                    sb.Append(renderBuffer[x, y]);
+                }
+                sb.AppendLine();
+            }
+            Console.Write(sb.ToString());
+
+            string hearts = "";
+            for (int i = Lives; i > 0; i--)
+            {
+                hearts += heartSymbol;
+            }
+
+            Console.SetCursorPosition(0, Field.Height + 1);
+            Console.WriteLine("Collect all the stars to win!");
+            Console.WriteLine("Avoid enemy bullets if you want to survive.");
+            Console.WriteLine($"\nLevel: {Level}/3");
+            Console.WriteLine($"\nScore: {_score}/5");
+            Console.WriteLine($"\nLives: {Lives} {hearts.PadRight(100)}");
+            Console.WriteLine("\nPress 'F10' to save your progress.");
+        }
+    }
+
+    public static void Restart()
+    {
+
+        Console.Clear();
+
+        StopThreads();
+
+        Lives = 5;
+        _score = 0;
+        _field = new Field(Level);
+        _player.X = 2; _player.Y = 2;
+        _player = new Player(_field);
+        _enemy.X = Field.Width - 4; _enemy.Y = Field.Height - 4;
+
+        _enemy = new Enemy(_field);
+        _reward = new Reward(5);
+        _reward.SetRewards(_score, _field, _player);
+
+        GameState = State.Run;
+
+        StartEnemyMovement();
+        StartBulletMovement();
+        StartRendering();
+
+        RunGame();
+    }
+
+    private static void GenerateNewLevel()
+    {
+        Console.Clear();
+
+        StopThreads();
+
+        _score = 0;
+        _field = new Field(Level);
+        _player.X = 2; _player.Y = 2;
+        _player = new Player(_field);
+        _enemy.X = Field.Width - 4; _enemy.Y = Field.Height - 4;
+
+        _enemy = new Enemy(_field);
+        _reward = new Reward(5);
+        _reward.SetRewards(_score, _field, _player);
+
+        GameState = State.Run;
+
+        StartEnemyMovement();
+        StartBulletMovement();
+        StartRendering();
+
+        RunGame();
+    } 
+
+    public static void SaveGame(string filePath)
+    {
+        try
+        {
+            var gameData = new
+            {
+                Level = Level,
+                Score = _score,
+                Lives = Lives,
+                PlayerPosition = new { X = _player.X, Y = _player.Y },
+                EnemyPosition = new { X = _enemy.X, Y = _enemy.Y },
+                Rewards = _reward._rewardPositions.Select(pos => new { X = pos.Item1, Y = pos.Item2 }).ToList(),
+                Field = _field.FieldGrid.Cast<BaseObject>().Select(obj => new
+                {
+                    X = obj.X,
+                    Y = obj.Y,
+                    Symbol = obj.Symbol
+                }).ToList()
+            };
+
+            var json = JsonSerializer.Serialize(gameData, new JsonSerializerOptions { WriteIndented = true });
+
+            File.WriteAllText(filePath, json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed to save game: {ex.Message}");
+        }
+    }
+    public static void LoadGame(string filePath)
+    {
+        var json = File.ReadAllText(filePath);
+        var gameData = JsonSerializer.Deserialize<JsonElement>(json);
+
+        Level = gameData.GetProperty("Level").GetInt32();
+        _score = gameData.GetProperty("Score").GetInt32();
+        Lives = gameData.GetProperty("Lives").GetInt32();
+        _player.X = gameData.GetProperty("PlayerPosition").GetProperty("X").GetInt32();
+        _player.Y = gameData.GetProperty("PlayerPosition").GetProperty("Y").GetInt32();
+        _enemy.X = gameData.GetProperty("EnemyPosition").GetProperty("X").GetInt32();
+        _enemy.Y = gameData.GetProperty("EnemyPosition").GetProperty("Y").GetInt32();
+
+        foreach (var reward in gameData.GetProperty("Rewards").EnumerateArray())
+        {
+            int rewardX = reward.GetProperty("X").GetInt32();
+            int rewardY = reward.GetProperty("Y").GetInt32();
+            _reward._rewardPositions.Add((rewardX, rewardY));
+            _field.FieldGrid[rewardX, rewardY] = new Reward(1);
+        }
+
+        _field = new Field(Level);
+        foreach (var obj in gameData.GetProperty("Field").EnumerateArray())
+        {
+            int x = obj.GetProperty("X").GetInt32();
+            int y = obj.GetProperty("Y").GetInt32();
+            string symbol = obj.GetProperty("Symbol").GetString();
+
+            if (symbol == Field.FieldBase)
+                _field.FieldGrid[x, y] = new EmptyObject(x, y, Field.FieldBase);
+            else if (symbol == Field.FieldBarrier)
+                _field.FieldGrid[x, y] = new BarrierObject(x, y, Field.FieldBarrier);
+            else if (symbol == Reward._rewardSign)
+                _field.FieldGrid[x, y] = new Reward(1);
+            else if (symbol == Player.PlayerFigure)
+                _field.FieldGrid[x, y] = new Player(_field);
+            else if (symbol == Enemy._enemyFigure)
+                _field.FieldGrid[x, y] = new Enemy(_field);
+        }
+    }
+    private static void StartEnemyMovement()
+    {
+        _enemyMoveActive = true;
+
+        _enemyMoveThread = new Thread(() =>
+        {
+            try
+            {
+                while (_enemyMoveActive)
+                {
+                    lock (consoleLock)
+                    {
+                        Player.CheckCollision(_player, _enemy);
+                        _enemy.IsOverlapping(_enemy);
+                        _enemy.Move(_field, _player);
+                        _enemy.Shoot(_field, _player, _enemy);
+                    }
+
+                    Thread.Sleep(400);
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                
+            }
+        });
+
+        _enemyMoveThread.IsBackground = true;
+        _enemyMoveThread.Start();
+    }
+    private static void StartBulletMovement()
+    {
+        _bulletActive = true;
+
+        _bulletThread = new Thread(() =>
+        {
+            try
+            {
+                while (_bulletActive && GameState == State.Run)
+                {
+                    lock (consoleLock)
+                    {
+                        Bullet.UpdateBullets(_player.Bullets, _field, _reward, _enemy);
+                        Bullet.UpdateBullets(_enemy.Bullets, _field, _reward, _enemy);
+                        Bullet.HitPlayer(_enemy.Bullets, _player);
+                        Bullet.HitEnemy(_player.Bullets, _enemy, _field);
+                    }
+
+                    Thread.Sleep(200);
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+
+            }
+        });
+
+        _bulletThread.IsBackground = true;
+        _bulletThread.Start();
+    }
+    private static void StartRendering()
+    {
+        _renderActive = true;
+        _renderThread = new Thread(() =>
+        {
+            try
+            {
+                while (_renderActive)
+                {
+                    lock (consoleLock)
+                    {
+                        if (GameState == State.Run)
+                        {
+                            RenderAll();
+                        }
+                    }
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+
+            }
+        });
+        _renderThread.IsBackground = true;
+        _renderThread.Start();
+    }
+    private static void StopThreads()
+    {
+        _enemyMoveActive = false;
+        _bulletActive = false;
+        _renderActive = false;
+
+        _enemyMoveThread?.Interrupt(); 
+        _bulletThread?.Interrupt();
+        _renderThread?.Interrupt();
+
+        _enemyMoveThread?.Join();
+        _bulletThread?.Join();
+        _renderThread?.Join();
+    }
+    public static void RunGame()
+    {
+        while (GameState == State.Run)
+        {
+
+            _reward.CheckRewards(_score, _field);
+            _score += _reward.GetReward(_player.X, _player.Y, _field);
+
+            if (_score == _reward.NumberOfRewards)
+            {
+                if (Level == Field.NumberOfLevels)
+                {
+                    GameState = State.Win;
+                }
+                else
+                {
+                    Level++;
+                    GenerateNewLevel();
+                }
+            }
+
+            Player.PlayerActions(_player, _field, _reward, _enemy);
+
+            if (GameState == State.Win)
+            {
+                Console.Clear();
+                Menu.ShowWinMessage();
+                return;
+            }
+
+            if (GameState == State.Defeat || GameState == State.Pause)
+            {
+                Console.Clear();
+                Menu.ShowGameOver();
+            }
+
+            if (GameState == State.Save)
+            {
+                StopThreads();
+
+                Console.Clear();
+                Menu.SaveProgress();
+
+                StartEnemyMovement();
+                StartBulletMovement();
+                StartRendering();
+            }
+
+            if (GameState == State.Exit)
+            {
+                StopThreads();
+
+                Console.Clear();
+                Menu.ExitGame();
+
+                StartEnemyMovement();
+                StartBulletMovement();
+                StartRendering();
+            }
+
+            if (GameState == State.Load)
+            {
+                StartEnemyMovement();
+                StartBulletMovement();
+                StartRendering();
+
+                GameState = State.Run;
+            }
+
+            Thread.Sleep(10);
+        }
+    }
+}
